11163751b6394c5e4018ecba80af6c16b935cdf9
Luis <lz24069@gmail.com>
2020-08-19 17:27:28 -0400

    Updates

---
+++ precache-manifest.74dfc389663df79d673f3cc099a0dd89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b893036819c182d82919988b5ea4e27",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "e75050151759cd5451b7",
+    "url": "/markdown-previewer/static/css/main.010a4aa5.chunk.css"
+  },
+  {
+    "revision": "d6e14ba78def40d8918b",
+    "url": "/markdown-previewer/static/js/2.f35f9eaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/markdown-previewer/static/js/2.f35f9eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75050151759cd5451b7",
+    "url": "/markdown-previewer/static/js/main.014ba7bc.chunk.js"
+  },
+  {
+    "revision": "380a9cf99d31db8da525",
+    "url": "/markdown-previewer/static/js/runtime-main.4fd0b226.js"
+  }
+]);
