9ba03dee3144916a16c8c897b613dacf8ddae4e7
Luis <lz24069@gmail.com>
2021-02-08 14:58:47 -0500

    update color scheme and component layout

--- src/Components/Editor/Editor.js
+++ src/Components/Editor/Editor.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "./Editor.css";
-import "../Toolbar1/Toolbar1";
-import Toolbar1 from "../Toolbar1/Toolbar1";
 
 const Editor = ({ input, updateInput }) => {
 	const handleChange = (e) => updateInput(e.target.value);
 
 	return (
 		<div className="editor">
-			<Toolbar1 />
 			<textarea
 				value={input}
 				onChange={handleChange}
--- src/Components/Previewer/Previewer.js
+++ src/Components/Previewer/Previewer.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import marked from "marked";
 import "./Previewer.css";
-import "../Toolbar2/Toolbar2";
-import Toolbar2 from "../Toolbar2/Toolbar2";
 
 const Previewer = ({ raw_text }) => {
 	const getMarkdownText = (text) => {
@@ -15,7 +13,6 @@ const Previewer = ({ raw_text }) => {
 
 	return (
 		<div className="previewer">
-			<Toolbar2 />
 			<div
 				dangerouslySetInnerHTML={
 					raw_text ? getMarkdownText(raw_text) : { __html: "" }
